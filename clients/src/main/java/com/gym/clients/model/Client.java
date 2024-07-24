90ed9a3fc496b0b8302d4c2974571d278438fcb2
Szymon <szymon9715@gmail.com>
2024-07-24 10:48:42 +0200

    Add client's model

---
+++ clients/src/main/java/com/gym/clients/model/Client.java
@@ -0,0 +1,40 @@
+package com.gym.clients.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "idGenerator", initialValue = 1, allocationSize = 1)
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idGenerator")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
