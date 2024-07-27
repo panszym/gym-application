58a612cf7414242397f64ddad00a49835a378295
Szymon <szymon9715@gmail.com>
2024-07-27 11:48:02 +0200

    Add trainings service configuration

---
+++ trainings/src/test/java/com/gym/trainings/TrainingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.trainings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
