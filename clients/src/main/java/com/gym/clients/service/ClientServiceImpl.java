a633d20d4e130f889ffbdf490f76e2e15d88adcd
Szymon <szymon9715@gmail.com>
2024-07-30 21:21:39 +0200

    Add some tests

--- clients/src/main/java/com/gym/clients/service/ClientServiceImpl.java
+++ clients/src/main/java/com/gym/clients/service/ClientServiceImpl.java
@@ -4,12 +4,15 @@ import com.gym.clients.exception.ClientException;
 import com.gym.clients.exception.Error;
 import com.gym.clients.model.Client;
 import com.gym.clients.repository.ClientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ClientServiceImpl implements ClientService {
+    Logger logger = LoggerFactory.getLogger(ClientServiceImpl.class);
     private final ClientRepository clientRepository;
 
     public ClientServiceImpl(ClientRepository clientRepository) {
@@ -18,17 +21,20 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public List<Client> getClients() {
+        logger.info("getClients method.");
         return clientRepository.findAll();
     }
 
     @Override
     public Client getClientById(Long id) {
         Client client = clientRepository.findById(id).orElseThrow(() -> new ClientException(Error.CLIENT_NOT_FOUND));
+        logger.info("getClient by id method.");
         return client;
     }
 
     @Override
     public List<Client> getClientsByStatus(Client.Status status) {
+        logger.info("getClient by status method.");
         if (status != null) {
             return clientRepository.findAllByStatus(status);
         } else throw new ClientException(Error.THERE_IS_NOT_REQUIRED_PARAMETER_STATUS);
@@ -38,6 +44,7 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public List<Client> getClientsByTicket(Client.Ticket ticket) {
+        logger.info("getClient by ticket method.");
         if (ticket != null) {
             return clientRepository.findAllByTicket(ticket);
         } else throw new ClientException(Error.THERE_IS_NOT_REQUIRED_PARAMETER_TICKET);
@@ -45,11 +52,13 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public List<Client> getClientsByEmail(List<String> emails) {
+        logger.info("getClients by email method.");
         return clientRepository.findAllByEmailIn(emails);
     }
 
     @Override
     public Client addClient(Client client) {
+        logger.info("addClient method.");
         checkClientEmail(client);
         clientRepository.save(client);
         return client;
@@ -57,11 +66,14 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public void deleteClient(Long id) {
+        logger.info("deleteClient method.");
+        if (!clientRepository.existsById(id)) throw new ClientException(Error.CLIENT_NOT_FOUND);
         clientRepository.deleteById(id);
     }
 
     @Override
     public Client putClient(Client client, Long id) {
+        logger.info("putClient method.");
         return clientRepository.findById(id)
                 .map(dbClient -> {
                     if (!dbClient.getEmail().equals(client.getEmail())) {
@@ -74,6 +86,7 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public Client patchClient(Client client, Long id) {
+        logger.info("patchClient method.");
         return clientRepository.findById(id)
                 .map(dbClient -> {
                     dbClient.updateClientPatch(client);
@@ -83,6 +96,7 @@ public class ClientServiceImpl implements ClientService {
 
     @Override
     public void toggleStatus(Long id) {
+        logger.info("toggleStatus method.");
         Client client = clientRepository.findById(id).orElseThrow(() -> new ClientException(Error.CLIENT_NOT_FOUND));
         client.toggleStatus();
         clientRepository.save(client);
---
+++ clients/src/test/java/com/gym/clients/service/ClientServiceImplTest.java
@@ -0,0 +1,129 @@
+package com.gym.clients.service;
+
+import com.gym.clients.exception.ClientException;
+import com.gym.clients.model.Client;
+import com.gym.clients.repository.ClientRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ClientServiceImplTest {
+
+
+    @Test
+    void getClientById_clientIsNotInTheSystem_shouldThrowClientException() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        when(mockClientRepository.findById(anyLong())).thenReturn(Optional.empty());
+        var exception = catchThrowable(() -> toTest.getClientById(1L));
+        //then
+        assertThat(exception)
+                .isInstanceOf(ClientException.class);
+    }
+
+    @Test
+    void getClientById_clientIsInTheSystem_shouldGetClientById() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var mockClient = mock(Client.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        when(mockClientRepository.findById(anyLong())).thenReturn(Optional.of(mockClient));
+        var result = mockClientRepository.findById(1L);
+        //then
+        assertNotNull(result, "Response is null");
+    }
+
+    @Test
+    void getClientsByStatus_noStatus_shouldThrowClientException() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        var exception = catchThrowable(() -> toTest.getClientsByStatus(null));
+        //then
+        assertThat(exception)
+                .isInstanceOf(ClientException.class);
+    }
+
+    @Test
+    void getClientsByStatus_ActiveStatus_shouldReturnClientByStatus() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var mockClient1 = mock(Client.class);
+        var mockClient2 = mock(Client.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        when(mockClientRepository.findAllByStatus(Client.Status.ACTIVE)).thenReturn(List.of(mockClient1, mockClient2));
+        var result = toTest.getClientsByStatus(Client.Status.ACTIVE);
+        //then
+        assertNotNull(result, "Response is null");
+    }
+
+    @Test
+    void getClientsByTicket_noTicket_shouldThrowClientException() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        var exception = catchThrowable(() -> toTest.getClientsByTicket(null));
+        //then
+        assertThat(exception)
+                .isInstanceOf(ClientException.class);
+    }
+
+    @Test
+    void getClientsByTicket_PremiumTicket_shouldReturnClientByTicket() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var mockClient1 = mock(Client.class);
+        var mockClient2 = mock(Client.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        when(mockClientRepository.findAllByTicket(Client.Ticket.PREMIUM)).thenReturn(List.of(mockClient1, mockClient2));
+        var result = toTest.getClientsByTicket(Client.Ticket.PREMIUM);
+        //then
+        assertNotNull(result, "Response is null");
+    }
+
+    @Test
+    void getClientsByEmail() {
+    }
+
+    @Test
+    void addClient() {
+    }
+
+
+    @Test
+    void putClient() {
+    }
+
+    @Test
+    void patchClient() {
+    }
+
+    @Test
+    void toggleStatus_clientIsNotInSystem_shouldThrowClientException() {
+        //given
+        var mockClientRepository = mock(ClientRepository.class);
+        var toTest = new ClientServiceImpl(mockClientRepository);
+        //when
+        when(mockClientRepository.findById(anyLong())).thenReturn(Optional.empty());
+        var exception = catchThrowable(() -> toTest.getClientById(1L));
+        //then
+        assertThat(exception)
+                .isInstanceOf(ClientException.class);
+    }
+
+}
