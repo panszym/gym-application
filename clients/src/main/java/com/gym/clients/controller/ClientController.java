0ce3ffc04022692e252d77bb538afca8bc2c134c
Szymon <szymon9715@gmail.com>
2024-08-07 21:08:47 +0200

    Add registration functionality commit 5

--- clients/src/main/java/com/gym/clients/controller/ClientController.java
+++ clients/src/main/java/com/gym/clients/controller/ClientController.java
@@ -1,16 +1,11 @@
 package com.gym.clients.controller;
 
-import com.gym.clients.auth.AuthenticationRequest;
-import com.gym.clients.auth.AuthenticationResponse;
 import com.gym.clients.model.Client;
-import com.gym.clients.service.AuthService;
 import com.gym.clients.service.ClientService;
-import jakarta.annotation.security.RolesAllowed;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
